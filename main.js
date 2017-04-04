bd31aa5ced7a42219fb2a2401de42db768f8da8b
sarah niemiec <sroseniemiec@gmail.com>
2017-04-04 13:23:37 -0400

    made some progress have two words to guess

--- main.js
+++ main.js
@@ -1,1 +1,140 @@
-console.log("hello")
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  var guessBox = document.querySelector("#guessbox")
+  var guessButton = document.querySelector("#guessbutton")
+  var guessButtonTwo = document.querySelector("#guessbuttontwo")
+  var tryAgainSpot = document.querySelector("#tryagainspot")
+  var firstWordLetters = document.querySelector("#firstwordletters")
+  var letterZero = document.querySelector("#letterzero")
+  var letterOne = document.querySelector("#letterone")
+  var letterTwo = document.querySelector("#lettertwo")
+  var letterThree = document.querySelector("#letterthree")
+  var letterFour = document.querySelector("#letterfour")
+  var letterFive = document.querySelector("#letterfive")
+  var letterSix = document.querySelector("#lettersix")
+  var hintSpot = document.querySelector("#hintspot")
+  var hintButton = document.querySelector("#hintbutton")
+  var hintButtonTwo = document.querySelector("#hintbuttontwo")
+  var congratsSpot = document.querySelector("#congratsspot")
+  var newGameButton = document.querySelector("#newgamebutton")
+
+  var firstWord = "cactus"
+  var firstWordRay = firstWord.split("")
+  var secondWord = "zamboni"
+  var secondWordRay = secondWord.split("")
+  var thirdWord = "juxtaposition"
+  var thirdWordRay = thirdWord.split("")
+
+  guessButtonTwo.style.display = "none"
+  hintButtonTwo.style.display = "none"
+
+  guessButton.addEventListener("click", function () {
+    var guess = guessBox.value.toLowerCase()
+    for(var i = 0; i <= firstWordRay.length; i++){
+      var guessSpot = firstWordRay.indexOf(guess)
+      if (guessSpot === -1) {
+        tryAgainSpot.innerHTML = "that letter isn't in the word...try again!"
+      } else if (guessSpot === 0){
+        letterZero.innerHTML = firstWordRay[0]
+        letterTwo.innerHTML = firstWordRay[2]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 1){
+        letterOne.innerHTML = firstWordRay[1]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 2){
+        letterTwo.innerHTML = firstWordRay[2]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 3){
+        letterThree.innerHTML = firstWordRay[3]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 4){
+        letterFour.innerHTML = firstWordRay[4]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 5){
+        letterFive.innerHTML = firstWordRay[5]
+        tryAgainSpot.innerHTML = "good guess"
+      }
+
+    }
+
+  })
+
+  hintButton.addEventListener("click", function () {
+    hintSpot.innerHTML = "I live in the desert."
+  })
+
+  newGameButton.addEventListener("click", function () {
+    tryAgainSpot.innerHTML = ""
+    hintSpot.innerHTML = ""
+    letterZero.innerHTML = "_"
+    letterOne.innerHTML = "_"
+    letterTwo.innerHTML = "_"
+    letterThree.innerHTML = "_"
+    letterFour.innerHTML = "_"
+    letterFive.innerHTML = "_"
+    letterSix.innerHTML = "_"
+    guessBox.value = ""
+    guessButton.style.display = "none"
+    hintButton.style.display = "none"
+    guessButtonTwo.style.display = "inline"
+    hintButtonTwo.style.display = "inline"
+
+  })
+
+  guessButtonTwo.addEventListener("click", function () {
+    var guess = guessBox.value.toLowerCase()
+    for(var i = 0; i <= secondWordRay.length; i++){
+      var guessSpot = secondWordRay.indexOf(guess)
+      if (guessSpot === -1) {
+        tryAgainSpot.innerHTML = "that letter isn't in the word...try again!"
+      } else if (guessSpot === 0){
+        letterZero.innerHTML = secondWordRay[0]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 1){
+        letterOne.innerHTML = secondWordRay[1]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 2){
+        letterTwo.innerHTML = secondWordRay[2]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 3){
+        letterThree.innerHTML = secondWordRay[3]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 4){
+        letterFour.innerHTML = secondWordRay[4]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 5){
+        letterFive.innerHTML = secondWordRay[5]
+        tryAgainSpot.innerHTML = "good guess"
+      } else if (guessSpot === 6){
+        letterSix.innerHTML = secondWordRay[6]
+        tryAgainSpot.innerHTML = "good guess"
+      }
+
+    }
+
+  })
+
+  hintButtonTwo.addEventListener("click", function () {
+    hintSpot.innerHTML = "I spend most of my time on the rink"
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
